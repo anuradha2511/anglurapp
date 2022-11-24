7b133b172ea9508c557c97b52b533fa3d5772dbf
anuradha2511 <anuradhapatil251999@gmail.com>
2022-11-24 09:13:32 +0530

    app component
    app component

---
+++ angularapp/src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-WarningAlert',
+    template: '<h2> This is a warning, you are in danger! </h2>',
+    styleUrls: ['./WarningAlert.component.css']
+})
+export class WarningAlertComponent{
+
+}
