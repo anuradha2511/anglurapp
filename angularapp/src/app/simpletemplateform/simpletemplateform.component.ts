b024aa3fafca46f720a4f1930cbaac115a186459
anuradha2511 <anuradhapatil251999@gmail.com>
2022-12-09 09:44:28 +0530

    template driven form
    template driven form

--- angularapp/src/app/simpletemplateform/simpletemplateform.component.ts
+++ angularapp/src/app/simpletemplateform/simpletemplateform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-simpletemplateform',
@@ -8,12 +9,29 @@ import { NgForm } from '@angular/forms';
 })
 export class SimpletemplateformComponent implements OnInit {
 
+  defaultValue: string = 'Angular';
+ employee = new Employee();
+ submitted: boolean =false;
   constructor() { }
 
   ngOnInit() {
   }
 OnSubmit(form:NgForm){
-console.log(form);
+  this.submitted = true;
+  this.employee.course = form.value.course;
+  this.employee.username = form.value.userDetails.username;
+  this.employee.email = form.value.userDetails.email;
+  
 
+this.saveEmployeeData(this.employee);
+// console.log(form.value.course);
+// console.log(form.value.userDetails.username);
+// console.log(form.value.userDetails.email);
+
+
+}
+saveEmployeeData(emp:Employee){
+  console.log(emp.course);
+  
 }
 }
